149630590157834529bb2c92ffdcde9ce9511d29
Tuan Nguyen <tuannguyen@reesoft.vn>
2018-02-17 12:55:19 +0700

    layout rev init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LeftmenuComponent } from './layout/leftmenu/leftmenu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LeftmenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/leftmenu/leftmenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rev-leftmenu',
+  templateUrl: './leftmenu.component.html',
+  styleUrls: ['./leftmenu.component.scss']
+})
+export class LeftmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
